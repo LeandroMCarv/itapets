dbbbb4562240fe1d2e8654ed2c0dbddee69ea06f
LeandroMCarv <leandroocarvalhoo9@gmail.com>
2025-10-29 09:42:22 -0300

    feat: limpa os campos após o envio do feedback

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function mensagemFeedback(){
     let mensagem = "Feedback enviado! Agradecemos sua contribuição.";
     document.getElementById("mensagem").textContent = mensagem;
+
+    //Captura os valores do formulário
+    let inputNome = document.getElementById("name");
+    let inputEmail = document.getElementById("email");
+    let inputTelefone = document.getElementById("tel");
+    let inputComentarios = document.getElementById("comentarios");
+
+    //Limpar os campos do formulários ao clicar em enviar
+    inputNome.value = "";
+    inputEmail.value = "";
+    inputTelefone.value = "";
+    inputComentarios.value = "";
 }
